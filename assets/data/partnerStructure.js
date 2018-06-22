2e9572d3509cb9b0d5ff6f3c3670daede212cb4a
Ambareesh Balaji <ambareeshbalaji@gmail.com>
2018-06-22 00:03:14 -0400

    Rename sponsors to partners, add links

---
+++ assets/data/partnerStructure.js
@@ -0,0 +1,211 @@
+export const hypersonic = [
+  {
+    name: 'Balluff',
+    img: 'hypersonic/Balluff.svg',
+    link: 'https://www.balluff.com/',
+  },
+  {
+    name: 'Altium',
+    img: 'hypersonic/altium.svg',
+    link: 'https://www.altium.com/',
+  },
+  {
+    name: 'Boko Design',
+    img: 'hypersonic/BOKO_DESIGN.png',
+    link: 'https://www.boko.ca/',
+  },
+  {
+    name: 'Communitech',
+    img: 'hypersonic/Communitech.svg',
+    link: 'https://www.communitech.ca/',
+  },
+  {
+    name: 'Eagle Cad',
+    img: 'hypersonic/EAGLECAD.svg',
+    link: 'https://www.autodesk.com/products/eagle/overview',
+  },
+  {
+    name: 'Infinity Testing Solutions',
+    img: 'hypersonic/ITS.svg',
+    link: 'http://www.infinitytesting.ca/',
+  },
+  {
+    name: 'Miltera',
+    img: 'hypersonic/Miltera.png',
+    link: 'http://miltera.com/',
+  },
+  {
+    name: 'Waterloo Engineering Endowment Foundation',
+    img: 'hypersonic/UW_ENGINEERING_ENDOWMENT.png',
+    link: 'http://www.weef.uwaterloo.ca/',
+  },
+  {
+    name: 'University of Waterloo Engineering Society',
+    img: 'hypersonic/UW_ENGINEERING_SOCIETY.png',
+    link: 'https://www.engsoc.uwaterloo.ca/',
+  },
+  {
+    name: 'University of Waterloo Engineering',
+    img: 'hypersonic/UW_Engineering.png',
+    link: 'https://uwaterloo.ca/engineering/',
+  },
+]
+
+export default [
+  {
+    name: 'Hypersonic',
+    members: hypersonic,
+  }, {
+    name: 'Supersonic',
+    members: [
+      {
+        name: 'Altair',
+        img: 'supersonic/Altair.svg',
+        link: 'https://www.altair.com/',
+      },
+      {
+        name: 'iDream Lab',
+        img: 'supersonic/idream.png',
+        link: 'http://i-dreamlab.com/',
+      },
+      {
+        name: 'League Health',
+        img: 'supersonic/LEAGUE_HEALTH.svg',
+        link: 'https://league.com/ca/',
+      },
+      {
+        name: 'Legett Platt',
+        img: 'supersonic/LEGETT_PLATT.svg',
+        link: 'http://leggett.com/',
+      },
+      {
+        name: 'Paypal',
+        img: 'supersonic/PAYPAL.svg',
+        link: 'https://www.paypal.com/ca/home',
+      },
+      {
+        name: 'Routes Transport Group',
+        img: 'supersonic/Routes_Transport_Group.png',
+        link: 'http://routestransport.com/',
+      },
+    ],
+  }, {
+    name: 'Transonic',
+    members: [
+      {
+        name: 'Arts Student Union',
+        img: 'transonic/Arts_Student_Union_nonEPS.svg',
+        link: 'https://uwaterloo.ca/arts-student-union/',
+      },
+      {
+        name: 'Deloitte',
+        img: 'transonic/deloitte.png',
+        link: 'https://www2.deloitte.com/',
+      },
+      {
+        name: 'IBI Group',
+        img: 'transonic/IBI.svg',
+        link: 'https://www.ibigroup.com/',
+      },
+      {
+        name: 'Javelin',
+        img: 'transonic/Javelin_nonEPS.svg',
+        link: 'https://www.javelin-tech.com/',
+      },
+      {
+        name: 'Kik Interactive',
+        img: 'transonic/KIK.svg',
+        link: 'https://www.kik.com/',
+      },
+      {
+        name: 'Maplesoft',
+        img: 'transonic/Maplesoft.svg',
+        link: 'https://www.maplesoft.com/',
+      },
+      {
+        name: 'Masri O Architects',
+        img: 'transonic/MASRI_O.svg',
+        link: 'http://masrioarchitects.ca/',
+      },
+      {
+        name: 'Naylor',
+        img: 'transonic/Naylor.svg',
+        link: 'https://www.naylor.com/',
+      },
+      {
+        name: 'University of Waterloo Architectural Engineering',
+        img: 'transonic/UW_ENGINEERING_ARCHITECTURE.png',
+        link: 'https://uwaterloo.ca/architectural-engineering/',
+      },
+      {
+        name: 'University of Waterloo Science',
+        img: 'transonic/UW_SCIENCE.png',
+        link: 'https://uwaterloo.ca/science/',
+      },
+      {
+        name: 'University of Waterloo',
+        img: 'transonic/uw-01.svg',
+        link: 'https://uwaterloo.ca/',
+      },
+      {
+        name: 'M Moser Associates',
+        img: 'transonic/MMOSER.svg',
+        link: 'https://www.mmoser.com/en/',
+      },
+    ],
+  }, {
+    name: 'Sonic',
+    members: [
+      {
+        name: 'Babylon VR',
+        img: 'sonic/BABYLON_VR.png',
+        link: 'https://www.webvirtualtours.com/',
+      },
+      {
+        name: 'Dassault Systèmes',
+        img: 'sonic/DASSAULT.png',
+        link: 'https://www.3ds.com/',
+      },
+      {
+        name: 'Groveware Technologies',
+        img: 'sonic/GROVEWARE_TECHNOLOGIES.png',
+        link: 'http://www.groveware.com/',
+      },
+      {
+        name: 'Ink Smith',
+        img: 'sonic/INK_SMITH.svg',
+        link: 'https://www.inksmith.co/',
+      },
+      {
+        name: 'LOT',
+        img: 'sonic/LOT41.png',
+        link: 'null',
+      },
+      {
+        name: 'Mitutoyo',
+        img: 'sonic/MITUTOYO.svg',
+        link: 'https://www.mitutoyo.com/',
+      },
+      {
+        name: 'Phidon',
+        img: 'sonic/PHIDON.png',
+        link: 'http://www.phidonpens.com/',
+      },
+      {
+        name: 'Sustainable.TO',
+        img: 'sonic/SUSTAINABLE_TO.svg',
+        link: 'https://www.sustainable.to/',
+      },
+      {
+        name: 'Silver Star',
+        img: 'sonic/SILVER_STAR.jpg',
+        link: 'http://www.silverstarmetal.com/',
+      },
+      {
+        name: 'Society of Waterloo Architecture Graduates',
+        img: 'sonic/UW_ARCHITECTURE_GRADUATE_SOCIETY.png',
+        link: 'https://uwaterloo.ca/architecture/current-graduate-students/swag',
+      },
+    ],
+  },
+]
