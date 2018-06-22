8350f134e1e5300f6cc6d4679be599d729ebdfd4
Ambareesh Balaji <ambareeshbalaji@gmail.com>
2018-06-22 00:54:44 -0400

    Add "why hyperloop", and some image and content changes

---
+++ components/Cover.vue
@@ -0,0 +1,27 @@
+<template>
+  <section
+    class="vh-100 nav-gap relative overflow-hidden flex white cover-shadow">
+    <Parallax class="absolute-bg">
+      <img
+        ref="img"
+        v-bind="$attrs"
+        :style="imgStyle"
+        :class="imgClass"
+        class="responsive-img">
+    </Parallax>
+    <slot/>
+  </section>
+</template>
+
+<script>
+/* eslint-disable vue/require-prop-types */
+
+export default {
+  inheritAttrs: false,
+  props: [
+    'imgClass',
+    'imgStyle',
+  ],
+}
+</script>
+
---
+++ components/MainArrow.vue
@@ -0,0 +1,32 @@
+<template>
+  <a
+    href="#main"
+    class="db arrow"/>
+</template>
+
+<style>
+.arrow {
+  animation: sdb 1.5s infinite;
+  border-bottom: 1px solid #fff;
+  border-left: 1px solid #fff;
+  height: 1.5rem;
+  width: 1.5rem;
+}
+
+@keyframes sdb {
+  0% {
+    opacity: 0;
+    transform: translate(0, 0) rotate(-45deg);
+  }
+
+  50% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+    transform: translate(0, 1rem) rotate(-45deg);
+  }
+}
+</style>
+
