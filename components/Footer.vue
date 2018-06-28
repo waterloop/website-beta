15e4d49a88c1c47308f3621423630a983d2e2b7e
Ziyao Tian <trista@v1020-wn-11-96.campus-dynamic.uwaterloo.ca>
2018-06-27 20:42:31 -0400

    Add footer icons

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,31 @@
 <template>
-  <footer class="page-footer grey lighten-5 white pv4">
-    <div class="container flex justify-between items-center"/>
+  <footer class="grey lighten-5 white">
+    <div class="container flex justify-between items-center h-100">
+      <div align="center">© 2018 Waterloop.</div> 
+      <div class="flex noseldrag space2">
+      <a
+        class="dim"
+        href="https://www.facebook.com/teamwaterloop"
+        target="_blank">
+        <img
+          src="~/assets/img/index/facebook.svg"
+          alt="team waterloop facebook"
+          height="30"></a>
+      <a
+        class="dim"
+        href="https://twitter.com/team_waterloop"
+        target="_blank"><img
+          src="~/assets/img/index/twitter.svg"
+          alt="team waterloop twitter"
+          height="30"></a>
+      <a
+        class="dim"
+        href="https://www.instagram.com/team_waterloop"
+        target="_blank"><img
+          src="~/assets/img/index/instagram.svg"
+          alt="team waterloop insta"
+          height="30"></a></div>
+    </div>
   </footer>
 </template>
 
@@ -8,6 +33,7 @@
 @import '~/assets/css/variables.scss';
 
 footer {
+  height: $nav-height;
   background: $dark-bg;
 }
 </style>
