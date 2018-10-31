dfce9dc512b440617131a5f9014e099212b7230e
Amby Balaji <ambareesh.balaji@globality.com>
2018-10-30 18:16:32 -0700

    Fix lint error

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,30 +1,30 @@
 <template>
   <footer class="grey lighten-5 white">
     <div class="container flex justify-between items-center h-100">
-      <div align="center">© 2018 Waterloop.</div> 
+      <div align="center">© 2018 Waterloop.</div>
       <div class="flex noseldrag space2">
-      <a
-        class="dim"
-        href="https://www.facebook.com/teamwaterloop"
-        target="_blank">
-        <img
-          src="~/assets/img/index/facebook.svg"
-          alt="team waterloop facebook"
-          height="30"></a>
-      <a
-        class="dim"
-        href="https://twitter.com/team_waterloop"
-        target="_blank"><img
-          src="~/assets/img/index/twitter.svg"
-          alt="team waterloop twitter"
-          height="30"></a>
-      <a
-        class="dim"
-        href="https://www.instagram.com/team_waterloop"
-        target="_blank"><img
-          src="~/assets/img/index/instagram.svg"
-          alt="team waterloop insta"
-          height="30"></a></div>
+        <a
+          class="dim"
+          href="https://www.facebook.com/teamwaterloop"
+          target="_blank">
+          <img
+            src="~/assets/img/index/facebook.svg"
+            alt="team waterloop facebook"
+            height="30"></a>
+        <a
+          class="dim"
+          href="https://twitter.com/team_waterloop"
+          target="_blank"><img
+            src="~/assets/img/index/twitter.svg"
+            alt="team waterloop twitter"
+            height="30"></a>
+        <a
+          class="dim"
+          href="https://www.instagram.com/team_waterloop"
+          target="_blank"><img
+            src="~/assets/img/index/instagram.svg"
+            alt="team waterloop insta"
+            height="30"></a></div>
     </div>
   </footer>
 </template>
@@ -33,7 +33,7 @@
 @import '~/assets/css/variables.scss';
 
 footer {
-  height: $nav-height;
   background: $dark-bg;
+  height: $nav-height;
 }
 </style>
