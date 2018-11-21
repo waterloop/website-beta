0e726f03591650cf7a42f4ca7e84f905b0e99bd5
Amby Balaji <ambareesh.balaji@globality.com>
2018-11-21 13:34:43 -0800

    Lint and cleanup

--- components/Post.vue
+++ components/Post.vue
@@ -1,63 +1,66 @@
 <!--<div v-if="tweet.entities.media[0].type = 'photo'"> -->
 
 <template>
-    <div class="social-card">
-        <div>
-            <img 
-            max-height= 50px;
-            max-width= 50px;
-            src={{twitterLogo}}> 
-            
-            <!-- <img src="this.extended_entities.media[0].media_url_https">  -->
-        </div>
-        <div class="center" v-if='twitter'>
-            <img :src="twitterLogo" /> 
-        </div>
-        <div class="description">
-            <h3>Waterloop</h3>
-            <p>{{ content }}</p>
-            <p2>@team_waterloop</p2>
-        </div>
-        <div class="date">
-            
-            <p>{{ date }}</p>   
-        </div>
-           
-        <!--hyperlinks to facebook and twitter -->
+  <div class="social-card">
+    <div>
+      <img
+        max-height= "50px;"
+        max-width= "50px;"
+        src="{{twitterLogo}}">
+
+        <!-- <img src="this.extended_entities.media[0].media_url_https">  -->
     </div>
+    <div
+      v-if="twitter"
+      class="center">
+      <img :src="twitterLogo" >
+    </div>
+    <div class="description">
+      <h3>Waterloop</h3>
+      <p>{{ content }}</p>
+      <p2>@team_waterloop</p2>
+    </div>
+    <div class="date">
+
+      <p>{{ date }}</p>
+    </div>
+
+    <!--hyperlinks to facebook and twitter -->
+  </div>
 </template>
 
 <script>
-import twitterLogo from '../assets/img/index/twitter.svg'
-import mediaPage from '../pages/media'
+/* eslint-disable vue/require-prop-types */
+// import twitterLogo from '../assets/img/index/twitter.svg'
+
 export default {
-    props : ['imag', 'mediaLogo', 'content', 'waterloopLogo', 'postDate', 'account'],
-    data() {
-        this.date = formatPostDate(this.postDate)
-        // this.photo = .....this.imag
-    }
+  props: [ 'imag', 'mediaLogo', 'content', 'waterloopLogo', 'postDate', 'account' ],
+  data() {
+    this.date = formatPostDate(this.postDate)
+    // this.photo = .....this.imag
+  },
 }
 
 
 
 
 function formatPostDate(postDate) {
-    let formattedDate;
-    let currentDate = new Date();
-    let timeDifference = Math.abs(currentDate.getTime() - postDate.getTime());
-    let differenceInDays = Math.ceil(timeDifference / 1000 * 3600 * 24);
+  let formattedDate
+  const currentDate = new Date()
+  const timeDifference = Math.abs(currentDate.getTime() - postDate.getTime())
+  const differenceInDays = Math.ceil(timeDifference / 1000 * 3600 * 24)
 
-    if(differenceInDays < 1) {
-        formattedDate = 'Today';
-    } else if(differenceInDays < 7) {
-        formattedDate = `${differenceInDays} days ago`
-    } else if(differenceInDays < 28) {
-        formattedDate = `${differenceInDays / 7} weeks ago`
-    } else {
-        formattedDate = postDate.toLocaleDateString();
-    }
+  if (differenceInDays < 1) {
+    formattedDate = 'Today'
+  } else if (differenceInDays < 7) {
+    formattedDate = `${ differenceInDays } days ago`
+  } else if (differenceInDays < 28) {
+    formattedDate = `${ differenceInDays / 7 } weeks ago`
+  } else {
+    formattedDate = postDate.toLocaleDateString()
+  }
 
-    return formattedDate
+  return formattedDate
 }
 </script>
 
@@ -68,35 +71,33 @@ function formatPostDate(postDate) {
   background-color: #fafafa;
   margin: 20px;
   width: 250px;
+}
 
 .description {
-    padding: 10px;
-    font-family: $body-font ;
-    > h3 {
-        text-align: center;
-        font-family: $body-font;
-        height: 45;
-        color: $brand-yellow;
-    }
-    > .p2 {
-        padding-top: 10px;
-        vertical-align: bottom;
-        text-align: center;
-        font-size: 5px;
-    }
+  padding: 10px;
+
+  > h3 {
+    align-self: center;
+    color: $brand-yellow;
+    height: 45;
+    text-align: center;
+  }
+
+  > .p2 {
+    font-size: 5px;
+    padding-top: 10px;
+    text-align: center;
+    vertical-align: bottom;
   }
 }
-.description h3 {
-    align-self: center;
-    font-family: $body-font;
-}
+
 .p {
-    align-self: center;
+  align-self: center;
 }
 
 .date {
-    color: grey;
-    text-align: center;
-    background-color: black;
+  background-color: #000;
+  color: #808080;
+  text-align: center;
 }
 </style>
