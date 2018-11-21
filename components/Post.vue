763ee9426246e56a01f9aa11c83bc43532bf2ee9
therealaugustine <spro0640@mylaurier.ca>
2018-11-21 15:27:20 -0500

    What I have so far

---
+++ components/Post.vue
@@ -0,0 +1,102 @@
+<!--<div v-if="tweet.entities.media[0].type = 'photo'"> -->
+
+<template>
+    <div class="social-card">
+        <div>
+            <img 
+            max-height= 50px;
+            max-width= 50px;
+            src={{twitterLogo}}> 
+            
+            <!-- <img src="this.extended_entities.media[0].media_url_https">  -->
+        </div>
+        <div class="center" v-if='twitter'>
+            <img :src="twitterLogo" /> 
+        </div>
+        <div class="description">
+            <h3>Waterloop</h3>
+            <p>{{ content }}</p>
+            <p2>@team_waterloop</p2>
+        </div>
+        <div class="date">
+            
+            <p>{{ date }}</p>   
+        </div>
+           
+        <!--hyperlinks to facebook and twitter -->
+    </div>
+</template>
+
+<script>
+import twitterLogo from '../assets/img/index/twitter.svg'
+import mediaPage from '../pages/media'
+export default {
+    props : ['imag', 'mediaLogo', 'content', 'waterloopLogo', 'postDate', 'account'],
+    data() {
+        this.date = formatPostDate(this.postDate)
+        // this.photo = .....this.imag
+    }
+}
+
+
+
+
+function formatPostDate(postDate) {
+    let formattedDate;
+    let currentDate = new Date();
+    let timeDifference = Math.abs(currentDate.getTime() - postDate.getTime());
+    let differenceInDays = Math.ceil(timeDifference / 1000 * 3600 * 24);
+
+    if(differenceInDays < 1) {
+        formattedDate = 'Today';
+    } else if(differenceInDays < 7) {
+        formattedDate = `${differenceInDays} days ago`
+    } else if(differenceInDays < 28) {
+        formattedDate = `${differenceInDays / 7} weeks ago`
+    } else {
+        formattedDate = postDate.toLocaleDateString();
+    }
+
+    return formattedDate
+}
+</script>
+
+<style lang='scss'>
+@import '~/assets/css/variables.scss';
+
+.social-card {
+  background-color: #fafafa;
+  margin: 20px;
+  width: 250px;
+
+.description {
+    padding: 10px;
+    font-family: $body-font ;
+    > h3 {
+        text-align: center;
+        font-family: $body-font;
+        height: 45;
+        color: $brand-yellow;
+    }
+    > .p2 {
+        padding-top: 10px;
+        vertical-align: bottom;
+        text-align: center;
+        font-size: 5px;
+    }
+  }
+}
+.description h3 {
+    align-self: center;
+    font-family: $body-font;
+}
+.p {
+    align-self: center;
+}
+
+.date {
+    color: grey;
+    text-align: center;
+    background-color: black;
+}
+</style>
